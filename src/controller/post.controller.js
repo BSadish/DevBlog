d5de16a9ca07d20a24e2d68d8c8d5e26e867acb5
BSadish <sadishpanta404@gmail.com>
2026-07-26 16:26:26 +0545

    added post controller to handler post logics

--- src/controller/post.controller.js
+++ src/controller/post.controller.js
@@ -1,17 +1,68 @@
-import { ApiError } from "../util/ApiError";
-import { asyncHandler } from "../util/asyncHandler";
+import { Post } from "../model/post.model.js";
+import { ApiError } from "../util/ApiError.js";
+import { ApiResponse } from "../util/ApiResponse.js";
+import { asyncHandler } from "../util/asyncHandler.js";
 
 
-export const userPost=asyncHandler(async(req,res)=>{
+export const createPost=asyncHandler(async(req,res)=>{
 
-const {title, content, tags, author, views}=req.body
-const {coverImage}=req.file
+const {title, content,author}=req.body
 
-if([title, content,author,tags].some(field=>field.trim()==="")){
+
+if([title,content].some(field=>(field).trim()==="")){
     throw new ApiError(401,"Content on all field are required")
 }
 
 
+const user=req.user
+if(!user){
+    throw new ApiError(401,"User is empty or not defined")
+}
 
 
+const userPost=await Post.create({
+    title,
+    content,
+    author:req.user._id
+    
+
+})
+
+return res.status(200)
+.json(new ApiResponse(200,userPost,"User created the post"))
+
+})
+
+export const getPostProfile=asyncHandler(async(req,res)=>{
+
+const currentUser=req.user
+const postUser=await Post.find({author:currentUser._id})
+
+
+
+return res.status(200)
+.json(new ApiResponse(200,postUser,"Get the post user"))
+
+
+})
+
+export const getPostById=asyncHandler(async(req,res)=>{
+
+const {postId}=req.params;
+console.log(req.params.postId)
+
+const post =await Post.findOneAndUpdate(
+    postId,
+    {
+        $inc:{views:1}},
+        {
+            returnDocument:"after"
+        }
+    
+)
+if(!post){
+    throw new ApiError(404,"Post not found")
+}
+return res.status(200)
+.json(new ApiResponse(200,post,"Fetched post"))
 })
