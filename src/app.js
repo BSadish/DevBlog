3c3d84e951ffda8634b0975fb9489d63bd522124
BSadish <sadishpanta404@gmail.com>
2026-07-26 16:26:13 +0545

    mounting postrouter

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 import express from "express"
 import userRouter from "./route/user.route.js"
+import cookieParser from "cookie-parser";
+import postRouter from "./route/post.route.js"
 const app=express();
 
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
-
+app.use(cookieParser())
 app.use("/api/v1/blog",userRouter)
+app.use("/api/v1/blog",postRouter)
 
 
 export {app}
