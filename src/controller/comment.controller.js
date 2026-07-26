21ed40f52a51658f8cf8d134e84ed9f0c07e8cb6
BSadish <sadishpanta404@gmail.com>
2026-07-26 20:48:37 +0545

    run comment controller successfully

--- src/controller/comment.controller.js
+++ src/controller/comment.controller.js
@@ -2,29 +2,29 @@ import { User } from "../model/user.model.js";
 import { ApiError } from "../util/ApiError.js";
 import { ApiResponse } from "../util/ApiResponse.js";
 import { asyncHandler } from "../util/asyncHandler.js";
-
+import {Comment} from "../model/comment.model.js"
 import { Post } from "../model/post.model.js";
 
 
 export const commentOnPost=asyncHandler(async(req,res)=>{
-    const {postId}=req.params;
+    const {id}=req.params;
     const {comment}=req.body
    const userId=req.user._id
-console.log(postId)
+console.log(id)
    if((comment).trim()===""){
     throw new ApiError(401,"No comment found")
    }
-
-   const post=await Post.findById(postId)
+// console.log(comment)
+   const post=await Post.findById(id)
    if(!post){
     throw new ApiError(401,"Post details not found")
    }
 
 
-    const userComment=await User.create({
+    const userComment=await Comment.create({
         comment,
         user:userId,
-        post:postId
+        post:id
     })
 
     return res.status(200)
