fb9d56a800d3d1bd1b7ff981dad8f95cf631a7b0
BSadish <sadishpanta404@gmail.com>
2026-07-26 16:26:49 +0545

    debug the response from cloudinary in user controller

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -13,7 +13,7 @@ export const generateAccessAndRefreshToken = async function (userid) {
 
         user.refreshToken = refreshToken
         await user.save({ validateBeforeSave: false })
-        return (accessToken, refreshToken)
+        return { accessToken, refreshToken }
     } catch (error) {
         throw new ApiError(500, error.message || "Error occurs while generateing access and refrehs token")
     }
@@ -23,14 +23,15 @@ export const generateAccessAndRefreshToken = async function (userid) {
 
 export const userRegister = asyncHandler(async (req, res) => {
 
-    const { username, password, eamil, avatar, bio } = req.body
 
-    if ([username, password, eamil, bio].some(field => field.trim() === "")) {
+    const { username, password, email, avatar, bio } = req.body
+    // console.log(req.body.email)
+    if ([username, password, email].some((field) => !field || field.trim() === "")) {
         throw new ApiError(401, "All field must be filled")
     }
 
     const existedUser = await User.findOne({
-        $or: { username, email }
+        $or: [{ username }, { email }]
     })
     if (existedUser) {
         throw new ApiError(401, "User with current name and email already existed")
@@ -59,21 +60,21 @@ export const userLogin = asyncHandler(async (req, res) => {
         throw new ApiError(401, "The form should not be empty")
     }
 
-    const existedUser = await User.findOne({
-        $or: { username, email }
+    const user = await User.findOne({
+        $or: [{ username }, { email }]
     })
-    if (!existedUser) {
+    if (!user) {
         throw new ApiError(401, "User with such credentials is not present")
     }
 
-    const user = await User.findById(existedUser._id)
 
-    const ispasswordValid = user.isPasswordCorrect(password)
+
+    const ispasswordValid = await user.isPasswordCorrect(password)
     if (!ispasswordValid) {
-        throw new ApiResponse(401, "User password doesnot match")
+        throw new ApiError(401, "User password doesnot match")
     }
 
-    const { accessToken, refreshToken } = generateAccessAndRefreshToken(user._id)
+    const { accessToken, refreshToken } = await generateAccessAndRefreshToken(user._id)
 
     const logIn = await User.findById(user._id).select("-password -refreshToken")
 
@@ -97,28 +98,30 @@ export const userProfile = asyncHandler(async (req, res) => {
 
 })
 
-export const userAvatar = asyncHandler(async (req, res) => {
+export const updateUserProfile = asyncHandler(async (req, res) => {
 
     const avatarLocalPath = req.file?.path;
+    console.log(avatarLocalPath)
     if (!avatarLocalPath) {
-        throw new ApiError(400, "Avatar file is required");
+        throw new ApiError(400, "Avatar file path not found");
 
     }
     //uploading to cloudinary
     const avatar = await uploadOnCloudinary(avatarLocalPath)
 
+    console.log(avatar)
     if (!avatar?.url) {
         throw new ApiError(400, "Avatar file is required");
     }
 
-   const updatedUser = await User.findByIdAndUpdate(
+    const updatedUser = await User.findByIdAndUpdate(
         req.user._id,
         {
             $set: { avatar: avatar.url }
         },
         { new: true }
     ).select("-password -refreshToken");
-    
+
     return res.status(200)
         .json(new ApiResponse(200, updatedUser, "Image uploaded successfully"))
 })
