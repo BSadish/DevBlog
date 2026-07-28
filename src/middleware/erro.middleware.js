6bab645b3156334bc1970048c83b2e5665a9c313
BSadish <sadishpanta404@gmail.com>
2026-07-28 22:05:41 +0545

    to handle error errormiddleware created

---
+++ src/middleware/erro.middleware.js
@@ -0,0 +1,20 @@
+import { ApiError } from "../util/ApiError.js";
+
+export const errorHandler=(err,req,res,next)=>{
+    let error=err;
+
+    if(!(error instanceof ApiError)){
+        const statusCode=error.statusCode || 500
+        const message=error.message || "Internal server error"
+        error= new ApiError(statusCode,message, error?.errors || [])
+    }
+
+    const response={
+        success:false,
+        message:error.message,
+        errors:error.errors,
+        ...(process.env.NODE_ENV)="development"? {stack:error.stack}:{}
+    };
+    return res.status(error.statusCode)
+    .json(response);
+}
