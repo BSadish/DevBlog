c35353c2cf20b35d58659ddc80e695cffb662d0f
BSadish <sadishpanta404@gmail.com>
2026-08-01 14:24:34 +0545

    run redis locally

--- src/db/redis.js
+++ src/db/redis.js
@@ -1,20 +1,36 @@
-import "dotenv/config"; 
-import Redis from "ioredis";
+// import "dotenv/config"; 
+// import Redis from "ioredis";
 
-const redisClient=new Redis({
-    username:"default",
-    password:process.env.REDIS_PASSWORD,
-    host:process.env.REDIS_HOST,
-    port:process.env.REDIS_PORT
-})
-
-
+// const redisClient=new Redis({
+//     username:"default",
+//     password:process.env.REDIS_PASSWORD,
+//     host:process.env.REDIS_HOST,
+//     port:process.env.REDIS_PORT
+// })
 
 
 
-redisClient.on("connect",()=>console.log("Connecting to Redis"))
-redisClient.on("ready", () => console.log("Redis is ready....."));
-redisClient.on("error", (err) => console.log("Redis Error..", err));
-redisClient.on("close", () => console.log("Redis connection is closed....."));
+
+
+
+// redisClient.on("connect",()=>console.log("Connecting to Redis"))
+// redisClient.on("ready", () => console.log("Redis is ready....."));
+// redisClient.on("error", (err) => console.log("Redis Error..", err));
+// redisClient.on("close", () => console.log("Redis connection is closed....."));
+
+// export default redisClient
+
+// const { createClient } = require("redis");
+import { createClient } from "redis";
+
+const redisClient = createClient();
+
+redisClient.on("error", (err) => {
+    console.log("Redis Error:", err);
+});
+
+(async () => {
+    await redisClient.connect();
+})();
 
 export default redisClient
